32622391950f92e6b086178ec3fdf6d8578953c6
GemaSoftware <41391455+GemaSoftware@users.noreply.github.com>
2023-05-01 16:15:17 -0400

    Added all code and comments.

--- app/src/main/java/com/agrongemajli/cse3200_ag_final/MainActivity.kt
+++ app/src/main/java/com/agrongemajli/cse3200_ag_final/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.agrongemajli.cse3200_ag_final
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
+import com.agrongemajli.cse3200_ag_final.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //instantiate the binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        //set the onClick for the Take Photo Button
+        binding.llTakePhotoButton.setOnClickListener {
+            //Set the intent to capture an image.
+            // This calls the camera app to take the photo
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            //With the results, pass that into the onActivityResukt function
+            startActivityForResult(cameraIntent, 200)
+        }
+        //Set the content view to activity_main
+        setContentView(binding.root)
     }
+
+    //This function comes from Google Camera Basics
+    //https://developer.android.com/training/camera-deprecated/photobasics
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //Check the result code is what we want for success and that we got an image back
+        if (resultCode == Activity.RESULT_OK && requestCode == 200 && data != null){
+
+            //Here we want some logic to move the "current" photo to previous and new into current.
+            //First, set previous photo = ImageBitmap data of Current Photo.
+            //Logic to check if null first.
+            if(binding.llImageView1.drawable != null)
+                binding.llImageView2.setImageBitmap(binding.llImageView1.drawable.toBitmap())
+
+            //Second, set the 'current photo' = data we just got from the camera.
+            binding.llImageView1.setImageBitmap(data.extras?.get("data") as Bitmap)
+
+        }
+    }
+
 }
