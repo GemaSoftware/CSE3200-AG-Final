f6bb8a84a761160a1a999c0c263e146675688355
GemaSoftware <41391455+GemaSoftware@users.noreply.github.com>
2023-05-01 14:45:16 -0400

    Initial commit

---
+++ app/src/main/java/com/agrongemajli/cse3200_ag_final/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.agrongemajli.cse3200_ag_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
